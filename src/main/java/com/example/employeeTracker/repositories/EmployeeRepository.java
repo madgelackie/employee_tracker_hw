7fa8c52aae6466c893ec83628a5e1c1c930747ba
madgelackie <marionmlackie@gmail.com>
2021-11-29 22:16:37 +0000

    file structure corrected and employee persisting to db

---
+++ src/main/java/com/example/employeeTracker/repositories/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.example.employeeTracker.repositories;
+
+import com.example.employeeTracker.models.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
--- src/test/java/com/example/employeeTracker/EmployeeTrackerApplicationTests.java
+++ src/test/java/com/example/employeeTracker/EmployeeTrackerApplicationTests.java
@@ -1,6 +1,10 @@
 package com.example.employeeTracker;
 
+
+import com.example.employeeTracker.models.Employee;
+import com.example.employeeTracker.repositories.EmployeeRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -8,6 +12,17 @@ class EmployeeTrackerApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Autowired
+	EmployeeRepository employeeRepository;
+
+	@Test
+	public void createEmployee(){
+		Employee employee1 = new Employee("Kate", 30, "RJWJ884884", "kate@email");
+		employeeRepository.save(employee1);
+		Employee employee2 = new Employee("Frank", 44, "EJ342292929", "frank@email");
+		employeeRepository.save(employee2);
 	}
 
 }
